40df493f175f3933866b44294eec4e6115d45577
Joseph <jmgrant1995@gmail.com>
2020-01-12 16:36:20 +0800

    Style footer responsively and add copyright notice

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,22 +12,27 @@ function Layout({ children }) {
 				{children}
 			</main>
 
-			<footer>
-				<nav className="flex justify-end max-w-4xl mx-auto p-4 md:p-8 text-sm">
-					<a
-						className="font-bold text-knight-black mr-4"
-						href="https://twitter.com/itsmejgrant"
-						rel="noopener noreferrer"
-						target="_blank">
-						Twitter
-					</a>
-					<a
-						className="font-bold hover:underline text-knight-black"
-						href="https://github.com/JMG1995/"
-						rel="noopener noreferrer"
-						target="_blank">
-						GitHub
-					</a>
+			<footer className="bg-space-cadet text-white">
+				<nav className="flex flex-col justify-center md:flex-row md:justify-between container mx-auto p-4 md:p-8 items-center text-base">
+					<small className="order-2 mt-4 md:mt-0 text-base">
+						Joseph Grant 2020 <sup>&copy;</sup>
+					</small>
+					<div className="flex justify-between md:order-2">
+						<a
+							className="mr-6 hover:underline"
+							href="https://twitter.com/itsmejgrant"
+							rel="noopener noreferrer"
+							target="_blank">
+							Twitter
+						</a>
+						<a
+							className="hover:underline"
+							href="https://github.com/JMG1995/"
+							rel="noopener noreferrer"
+							target="_blank">
+							GitHub
+						</a>
+					</div>
 				</nav>
 			</footer>
 		</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,13 +15,6 @@ module.exports = {
 			heading: [`Rubik`],
 			body: [`Karla`]
 		}
-		// fontSize: {
-		// 	xs: `.75rem`,
-		// 	sm: `.875rem`,
-		// 	base: `1rem`,
-		// 	lg: `1.125rem`,
-		// 	xl: `1.25rem`
-		// }
 	},
 	variants: {},
 	plugins: []
