6833390f9525eabdc83dead300653ebaf7d6b053
Joseph <jmgrant1995@gmail.com>
2020-01-14 21:31:07 +0800

    Add basic homepage content

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,13 +11,22 @@ function IndexPage() {
 				title="Home"
 			/>
 
-			<section className="text-center">
-				<h1>This is a heading</h1>
-				<h2>This is a heading</h2>
-				<h3>This is a heading</h3>
-				<h4>This is a heading</h4>
-				<h5>This is a heading</h5>
-				<h6>This is a heading</h6>
+			<section className="px-20 w-1/2">
+				<h2>
+					Hi! My name is <span className="text-desire">Joseph.</span>
+				</h2>
+				<p className="mb-3">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi et
+					sodales velit, at dignissim tortor. Donec consequat et augue
+					vestibulum scelerisque. Donec consectetur orci venenatis nisl
+					lobortis, sit amet laoreet lacus eleifend.
+				</p>
+				<p>
+					Interdum et malesuada fames ac ante ipsum primis in faucibus. In at
+					est sagittis, feugiat nisl vel, eleifend nibh. Etiam vehicula urna vel
+					justo scelerisque aliquet. Nulla tristique tellus vel convallis
+					luctus. Mauris nec ultrices ex. Nunc maximus gravida luctus.
+				</p>
 			</section>
 		</Layout>
 	);
